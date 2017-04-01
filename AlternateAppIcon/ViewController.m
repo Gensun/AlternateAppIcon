884eeda17ea98dfbe62c54ab353148d055209777
Gensun <1412912425@qq.com>
2017-04-01 15:56:17 +0800

    code

--- AlternateAppIcon/ViewController.m
+++ AlternateAppIcon/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSUInteger t ;
+}
 
 @end
 
@@ -17,6 +20,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(changeIcon:) userInfo:nil repeats:YES];
+}
+
+
+-(void)changeIcon:(NSTimer *)sender
+{
+    //是否支持 AlternateIcons
+    if (t == 2) {
+        t = 1;
+    }else{
+        t ++;
+    }
+    if ([[UIApplication sharedApplication] supportsAlternateIcons]) {
+        [[UIApplication sharedApplication] setAlternateIconName:[NSString stringWithFormat:@"Icon%lu",(unsigned long)t] completionHandler:^(NSError * _Nullable error) {
+            
+            if (error) {
+                NSLog(@"error = %@",error.localizedDescription);
+            }else{
+                NSLog(@"good job !");
+            }
+        }];
+    }
 }
 
 
@@ -24,6 +50,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 
 @end
